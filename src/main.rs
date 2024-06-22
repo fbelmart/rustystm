a0c6ee24c879df8b039296589a735677fd77971e
fbelmart <todobelmar@gmail.com>
2024-06-22 18:59:23 -0400

    Add cargo generated files

---
+++ src/main.rs
@@ -0,0 +1,19 @@
+#![no_std]
+#![no_main]
+
+use cortex_m::asm::nop;
+use cortex_m_rt::entry;
+use panic_halt as _;
+use rtt_target::{rprintln, rtt_init_print};
+
+#[entry]
+fn main() -> ! {
+    rtt_init_print!();
+    rprintln!("Empezando programa!");
+    loop {
+        rprintln!("Hola chinita!");
+        for _ in 0..100_000 {
+            nop();
+        }
+    }
+}
